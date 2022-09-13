64179e124d26d50ba4aac9f6436d1ba71c0c84de
MobScripts <113401404+MobScripts@users.noreply.github.com>
2022-09-12 20:42:58 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'V3CT0RBUG'
-description 'Mob gas station'
+description 'Mob Fueler System'
 version '1.0'
 
 server_script 'main.lua'
 
 mob_updater 'yes'
-mob_name '^2Mob Fueler
+mob_name '^2Mob Fueler System'
 mob_github 'https://github.com/MobScripts/mob_fueler'
 
